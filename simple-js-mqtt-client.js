281fdac50e7c0148026c9e672ee55f49cb661512
Alessandro Gnoli <tebemis@gmail.com>
2014-08-08 12:51:48 -0400

    Subscribe/Unsubscribe done

--- simple-js-mqtt-client.js
+++ simple-js-mqtt-client.js
@@ -1,7 +1,9 @@
 // Simple MQTT client
 
-var MQTTClient = (function () {
-	var mqtt = {};
+var MQTT = (function () {
+	var mqtt = {},
+		isConnected = false,
+		subscriptions = {};
 
 	function privateMethod() {
 		// ...
@@ -9,33 +11,59 @@ var MQTTClient = (function () {
 
 	// mqtt.moduleProperty = 1;
 	mqtt.connect = function (host, clientId) {
+		// Create client
 		client = new Messaging.Client(host, Number(1884), clientId);
-		// client.onConnectionLost =	// gotta implement this
+		// Register callbacks
+		client.onConnectionLost = function(){
+			isConnected = false;
+			// TODO try to reconnect
+		}
+		client.onMessageArrived = function (message) {
+			console.log("Received " + message.payloadString + " on channel " + message.destinationName);
+		}
+		// Connect
 		client.connect({onSuccess:function(){
+			isConnected = true;
 			console.debug("Connected to MQTT server")	
 		}});
 	};
 	
 	mqtt.disconnect = function() {
 		client.disconnect()
+		isConnected = false;
 		console.debug("Disconnected from MQTT server")
 	}
 	
-	mqtt.onMessageArrived = function (message) {
-		console.log("onMessageArrived: "+message.payloadString);
+	mqtt.isConnected = function() {
+		return isConnected
 	}
 	
 	mqtt.subscribe = function (channel, callback) {
-		mqtt.subscribe(channel);
+		var options = {};
+		options.qos = 0;
+		options.onSuccess = function() {
+			subscriptions[channel] = callback;
+			console.debug("Subscribed to channel " + channel);
+		}
+		client.subscribe(channel, options);
 	};
 	
 	mqtt.unsubscribe = function (channel) {
-		mqtt.unsubscribe(channel);
+		var options = {};
+		options.onSuccess = function() {
+			delete subscriptions[channel];
+			console.debug("Unsubscribed from channel " + channel);
+		}
+		client.unsubscribe(channel, options);
+	};
+	
+	mqtt.getSubscriptions = function() {
+		return Object.keys(subscriptions);
 	};
 	
 	mqtt.publish = function (channel, message) {
 	  message = new Messaging.Message(message);
-	  message.destinationName = "obama";
+	  message.destinationName = channel;
 	  client.send(message);
 	};
 	
