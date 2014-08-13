a2ee6933e56790f7532671f345b6f5580d7fcae2
Alessandro Gnoli <tebemis@gmail.com>
2014-08-13 10:22:58 -0500

    Fixed example, added docs, bumped version

---
+++ README.md
@@ -0,0 +1,9 @@
+# Simple JS MQTT client
+A simple asynchronous MQTT client for the web
+
+# Dowload
+We like to use [Bower](http://bower.io/). Simply type `bower install https://github.com/ltg-uic/simple-js-mqtt-client.git` in your project directory. This will also dowload all the dependencies.
+
+# Use
+See the [hello.html](https://github.com/ltg-uic/simple-js-mqtt-client/examples/hello.html) example.
+
--- simple-js-mqtt-client.js
+++ simple-js-mqtt-client.js
@@ -1,15 +1,20 @@
-// Simple MQTT client
+/**********************
+ * Simple MQTT client *
+ **********************/
 
 var MQTT = (function () {
 	var mqtt = {},
 		isConnected = false,
 		subscriptions = {};
-
-	function privateMethod() {
-		// ...
-	}
-
-	mqtt.connect = function (host, clientId) {
+	
+	/**
+	 * Connects to a MQTT broker and optionally executes a callback.
+	 *
+	 * @param {string} host - the hostname of the broker.
+	 * @param {string} clientId  - the unique name of this client.
+	 * @param {callback} [callback] - A function that is executed after a successful connection.
+	 */
+	mqtt.connect = function (host, clientId, callback) {
 		// Create client
 		client = new Messaging.Client(host, Number(1884), clientId);
 		// Register callbacks
@@ -19,15 +24,21 @@ var MQTT = (function () {
 		}
 		client.onMessageArrived = function (message) {
 			subscriptions[message.destinationName](message.payloadString)
-			console.debug("Received " + message.payloadString + " on channel " + message.destinationName);
+			console.debug("Message `" + message.payloadString + "` on channel `" + message.destinationName +"`");
 		}
 		// Connect
 		client.connect({onSuccess:function(){
 			isConnected = true;
 			console.debug("Connected to MQTT server")	
+			// If there is a callback defined, execute it
+			if (callback!==undefined)
+				callback()
 		}});
 	};
 	
+	/**
+	 * Disconnects from the MQTT client.
+	 */
 	mqtt.disconnect = function() {
 		client.disconnect()
 		isConnected = false;
@@ -35,10 +46,19 @@ var MQTT = (function () {
 		console.debug("Disconnected from MQTT server")
 	}
 	
+	/**
+	 * Checks if we are still connected to the MQTT broker.
+	 * @returns {Boolean} true if connected, false otherwise.
+	 */
 	mqtt.isConnected = function() {
 		return isConnected
 	}
 	
+	/**
+	 * Subscribes to a channel and registers a callback.
+	 * @param {string} channel  - the channel we are subscribing to.
+	 * @param {callback} - A function that is executed every time a message is received on that channel.
+	 */
 	mqtt.subscribe = function (channel, callback) {
 		var options = {};
 		options.qos = 0;
@@ -49,6 +69,10 @@ var MQTT = (function () {
 		client.subscribe(channel, options);
 	};
 	
+	/**
+	 * Unsubscribe from a channel.
+	 * @param {string} channel  - the channel we are unsibscribing from.
+	 */
 	mqtt.unsubscribe = function (channel) {
 		var options = {};
 		options.onSuccess = function() {
@@ -58,10 +82,19 @@ var MQTT = (function () {
 		client.unsubscribe(channel, options);
 	};
 	
+	/**
+	 * Lists all the channels we are currently subscribed to.
+	 * @returns {Array} a lists of all the channels we are currently subscribed to.
+	 */
 	mqtt.getSubscriptions = function() {
 		return Object.keys(subscriptions);
 	};
 	
+	/**
+	 * Publishes a message to a channel.
+	 * @param {string} channel  - the channel we are publishing to.
+	 * @param {string} message - the message we are publishing.
+	 */
 	mqtt.publish = function (channel, message) {
 	  message = new Messaging.Message(message);
 	  message.destinationName = channel;
